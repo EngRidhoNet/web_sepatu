d51078e02cbfc6653e1d02e230d4e1ea3cbd4eba
Ridho Aulia Rahman <144804261+EngRidhoNet@users.noreply.github.com>
2024-12-02 10:15:35 +0700

    mass assignment

--- app/Models/ProductTransaction.php
+++ app/Models/ProductTransaction.php
@@ -3,8 +3,49 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductTransaction extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'booking_trx_id',
+        'city',
+        'post_code',
+        'address',
+        'quantity',
+        'sub_total_amount',
+        'grand_total_amount',
+        'discount_amount',
+        'is_paid',
+        'shoe_id',
+        'shoe_size',
+        'promo_code_id',
+        'proof',
+    ];
+
+    public static function generateUniqueTrxId()
+    {
+        $prefix = 'SSEKO';
+        do {
+            $randomstring = $prefix . mt_rand(1000, 9999);
+        } while (self::where('booking_trx_id', $randomstring)->exists()); {
+            return $randomstring;
+        }
+    }
+
+    public function shoe()
+    {
+        return $this->belongsTo(Shoe::class, 'shoe_id');
+    }
+
+    public function promoCode()
+    {
+        return $this->belongsTo(PromoCode::class, 'promo_code_id');
+    }
 }
