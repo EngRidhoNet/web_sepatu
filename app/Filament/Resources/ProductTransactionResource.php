675292898ba396d2cae47973f27842ba91e96969
Ridho Aulia Rahman <144804261+EngRidhoNet@users.noreply.github.com>
2024-12-23 20:14:12 +0700

    repository

--- app/Filament/Resources/ProductTransactionResource.php
+++ app/Filament/Resources/ProductTransactionResource.php
@@ -2,16 +2,33 @@
 
 namespace App\Filament\Resources;
 
+use Filament\Forms;
+use App\Models\Shoe;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use App\Models\ProductTransaction;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Wizard;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\Wizard\Step;
+use Symfony\Component\Console\Input\Input;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\ProductTransactionResource\Pages;
 use App\Filament\Resources\ProductTransactionResource\RelationManagers;
-use App\Models\ProductTransaction;
-use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Models\PromoCode;
+use Filament\Actions\Action;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Livewire\Notifications;
+use Filament\Tables\Actions\Action as ActionsAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 
 class ProductTransactionResource extends Resource
 {
@@ -23,7 +40,152 @@ class ProductTransactionResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Wizard::make([
+                    Step::make('Product and Price')
+                        ->schema([
+                            Grid::make(2)
+                                ->schema([
+                                    Select::make('shoe_id')
+                                        ->relationship('shoe', 'name')
+                                        ->searchable()
+                                        ->preload()
+                                        ->reactive()
+                                        ->required()
+                                        ->afterStateUpdated(function ($state, callable $get, callable $set) {
+                                            $shoe = Shoe::find($state);
+                                            $price = $shoe ? $shoe->price : 0;
+                                            $quantity = $get('quantity') ?? 1;
+                                            $subTotalAmount = $price * $quantity;
+
+                                            $set('price', $price);
+                                            $set('sub_total_amount', $subTotalAmount);
+
+                                            $discount = $get('discount_amount') ?? 0;
+                                            $grandTotalAmount = $subTotalAmount - $discount;
+                                            $set('grand_total_amount', $grandTotalAmount);
+
+                                            $sizes = $shoe ? $shoe->sizes->pluck('size', 'id')->toArray() : [];
+                                            $set('shoe_sizes', $sizes);
+                                        })
+                                        ->afterStateHydrated(function (callable $get, callable $set, $state) {
+                                            $shoeId = $state;
+                                            if ($shoeId) {
+                                                $shoe = Shoe::find($shoeId);
+                                                $sizes = $shoe ? $shoe->sizes->pluck('size', 'id')->toArray() : [];
+                                                $set('shoe_sizes', $sizes);
+                                            }
+                                        }),
+
+                                    Select::make('shoe_size')
+                                        ->label('Shoe size')
+                                        ->options(function (callable $get) {
+                                            $sizes = $get('shoe_sizes');
+                                            return is_array($sizes) ? $sizes : [];
+                                        })
+                                        ->required()
+                                        ->reactive(),
+
+                                    TextInput::make('quantity')
+                                        ->required()
+                                        ->numeric()
+                                        ->prefix('Qty')
+                                        ->reactive()
+                                        ->afterStateUpdated(function ($state, callable $get, callable $set) {
+                                            $price = $get('price') ?? 0;
+                                            $quantity = $state;
+                                            $subTotalAmount = $price * $quantity;
+
+                                            $set('sub_total_amount', $subTotalAmount);
+
+                                            $discount = $get('discount_amount') ?? 0;
+                                            $grandTotalAmount = $subTotalAmount - $discount;
+                                            $set('grand_total_amount', $grandTotalAmount);
+                                        }),
+
+                                    Select::make('promo_code_id')
+                                        ->relationship('promoCode', 'code')
+                                        ->searchable()
+                                        ->preload()
+                                        ->reactive()
+                                        ->afterStateUpdated(function ($state, callable $get, callable $set) {
+                                            $subTotalAmount = $get('sub_total_amount');
+                                            $promoCode = PromoCode::find($state);
+                                            $discount = $promoCode ? $promoCode->discount : 0;
+
+                                            $set('discount_amount', $discount);
+
+                                            $grandTotalAmount = $subTotalAmount - $discount;
+                                            $set('grand_total_amount', $grandTotalAmount);
+                                        }),
+
+                                    TextInput::make('sub_total_amount')
+                                        ->required()
+                                        ->readOnly()
+                                        ->numeric()
+                                        ->prefix('IDR'),
+
+                                    TextInput::make('grand_total_amount')
+                                        ->required()
+                                        ->readOnly()
+                                        ->numeric()
+                                        ->prefix('IDR'),
+
+                                    TextInput::make('discount_amount')
+                                        ->required()
+                                        ->readOnly()
+                                        ->numeric()
+                                        ->prefix('IDR'),
+                                ])
+                        ]),
+
+                    Step::make('Customer')
+                        ->schema([
+                            Grid::make(2)
+                                ->schema([
+                                    TextInput::make('name')
+                                        ->required()
+                                        ->maxLength(255),
+                                    TextInput::make('phone')
+                                        ->required()
+                                        ->maxLength(255),
+                                    TextInput::make('email')
+                                        ->required()
+                                        ->email()
+                                        ->maxLength(255),
+                                    TextInput::make('address')
+                                        // ->rows(5)
+                                        ->required(),
+                                    TextInput::make('city')
+                                        ->required()
+                                        ->maxLength(255),
+                                    TextInput::make('post_code')
+                                        ->required()
+                                        ->maxLength(255),
+                                ])
+                        ]),
+
+                    Step::make('Payment Information')
+                        ->schema([
+                            TextInput::make('booking_trx_id')
+                                ->required()
+                                ->maxLength(255),
+                            ToggleButtons::make('is_paid')
+                                ->label('Apakah anda sudah membayar?')
+                                ->boolean()
+                                ->grouped()
+                                ->icons([
+                                    true => 'heroicon-o-check-circle',
+                                    false => 'heroicon-o-x-circle',
+                                ])
+                                ->required(),
+                            FileUpload::make('proof')
+                                ->image()
+                                ->required(),
+                        ]),
+                ])
+                ->columnSpan('full')
+                ->columns(1)
+                ->skippable(),
             ]);
     }
 
@@ -31,13 +193,39 @@ class ProductTransactionResource extends Resource
     {
         return $table
             ->columns([
-                //
+                ImageColumn::make('shoe.thumbnail'),
+                TextColumn::make('name'),
+                IconColumn::make('is_paid')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Terverifikasi')
+                  
             ])
             ->filters([
-                //
+                SelectFilter::make('shoe_id')
+                    ->label('Shoe')
+                    ->relationship('shoe', 'name')
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
+
+                Tables\Actions\Action::make('approve')
+                    ->label('Approve')
+                    ->action(function (ProductTransaction $record) {
+                        $record->is_paid = true;
+                        $record->save();
+
+                        Notifications::success('Transaction has been approved.');
+                    })
+                    ->color('success')
+                    ->requiresConfirmation()
+                    ->visible(function (ProductTransaction $record) {
+                        return !$record->is_paid;
+                    }),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
